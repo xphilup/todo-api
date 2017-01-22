93cef42c316d889d7f9c23793f7d36c77bc96f4f
Phillip M Hamilton <xphilup@hotmail.com>
2017-01-22 09:51:51 -0800

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: "Meet mom for lunch",
-	completed: false
-}, {
-	id: 2,
-	description: "Go to market",
-	completed: false
-}, {
-	id: 3,
-	description: "Study web code for 4 hours.",
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
+
+
 
 app.get('/', function(req, res) {
 	res.send('Todo API Root');
@@ -47,6 +42,20 @@ app.get('/todos/:id', function(req, res) {
 	}
 
 });
+
+
+//-------------------------------------------------
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	
+	todos.push(body);
+
+	res.json(body);
+});
+
 
 
 
